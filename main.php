086de3fe79cd9963c64497d57f007524312d9d03
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-03-07 16:30:24 +0000

    .

--- main.php
+++ main.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--<link rel="stylesheet" href="style.css"> -->
+        <link rel="stylesheet" href="main.css">
         <title>Cactus Soup</title>
         <?php include 'navbar.html'?>
     </head>
     <body>
+        ashfjahhgh
     </body>
 </html>
