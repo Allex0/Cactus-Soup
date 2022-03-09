d3811dd65221da3b7215b63d5e20bd7f51175677
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-03-09 14:58:32 +0000

    Update main.php

--- main.php
+++ main.php
@@ -3,12 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css\main.css">
         <title>Cactus Soup</title>
         <?php include 'navbar.html'?>
     </head>
     <body>
-
     <div style="overflow:auto">
         <div class="menu">
             <a href="#">Link 1</a>
