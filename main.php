b3672b7df496882819f83e73641eded68e5e8681
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-05-17 19:57:37 +0100

    -

--- main.php
+++ main.php
@@ -187,13 +187,7 @@ $_SESSION['LAST_ACTIVITY'] = $time;
             </div>
         
         </div>
-        
-        <div class="search">
-            <input type="text" class="searchTerm" placeholder="What are you looking for?">
-            <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
-            </button>
-        </div>
+                
         
     </body>
 </html>
