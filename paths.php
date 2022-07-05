c5fdec75c0c8939d94d0dbf7e47876a0501a06b3
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-07-05 15:26:45 +0100

    top filmes path

--- paths.php
+++ paths.php
@@ -2,6 +2,6 @@
 
     define("profile_page", "/cactus-soup/profile_page.php");
     define("main", "/cactus-soup/main.php");
-    define("logout", "/cactus-soup/logout.php")
-    
+    define("logout", "/cactus-soup/logout.php");
+    define("top_filmes", "/cactus-soup/top_filmes.php");
 ?>
