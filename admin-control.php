d8bd925206524eb2fb7b73f73031831699ae3233
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-07-07 17:11:33 +0100

    admin page to insert movies in db

---
+++ admin-control.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if (isset($_POST['upload'])) {
+
+  include 'login_register/config.php';
+
+  //$targetvid = "video-uploads/".basename($_FILES['video']['name']);
+  $target = "movies/images/".basename($_FILES['image']['name']);
+  $nome = $_POST['mname'];
+  $ano = $_POST['release'];
+  $keywords = $_POST['keywords'];
+  //$genre = strtolower($_POST['genre']);
+  //$rtime = $_POST['rtime'];
+  $descricao = utf8_decode($_POST['desc']);
+  $imagem = $_FILES['image']['name'];
+  //$video = $_FILES['video']['name'];
+
+  $sql = "INSERT INTO filmes (nome, ano, keywords, descricao, imgpath)
+    VALUES('$nome','$ano','$keywords','$descricao','$target')";
+
+  mysqli_query($conn,$sql);
+
+  if (move_uploaded_file($_FILES['image']['tmp_name'],$target)) {
+    header("Location: profile_page.php");
+  }else {
+    echo "error uploading";
+  }
+}
+
+
+?>
---
+++ admin.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])){
+
+}
+else {
+    echo "<script language='javascript'>alert('Nao esta na conta!');window.location.assign('main.php');</script>";
+}
+
+if ($_SESSION['role'] == 'admin'){
+
+}
+else {
+    echo "<script language='javascript'>alert('Nao tem acesso!');window.location.assign('main.php');</script>";
+}
+
+?>
+
+
+<!DOCTYPE html>
+
+<head>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/profile_page_form.css">
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <?php include 'navbar.php'; ?>
+</head>
+
+<body>
+    <div class="container-profile">
+        <div class="box-profile">
+            <div class="box-nested-principal">
+                <div class="box-text-principal">
+                    <p>Adicionar filme na base de dados</p>
+                </div>
+            </div>
+            <div class="profile-page-form">
+                <form class="" action="admin-control.php" method="POST" enctype="multipart/form-data">
+
+                    <input type="text" class="form-control" placeholder="Nome do filme" name="mname" value=""><br>
+                    <input type="text" class="form-control" placeholder="Ano" name="release" value="">
+                    <br>
+                    <input type="text" class="form-control" placeholder="Keywords (divididos por espaço)" name="keywords" value="">
+                    <br>
+                    <input type="text" class="form-control" placeholder="Descrição" name="desc" value="">
+                    <br>
+                    <div class="row">
+                        <div class="col">
+                            <table>
+                                <tr>
+                                    <td> <label for=""><b>Inserir imagem (.JPG) : </b></label> </td>
+                                    <td>
+                                        <div class="">
+                                            <input type="hidden" name="size" value="100000">
+
+                                            <input type="file" name="image" value="">
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+                    </div> <br><br>
+                    <div class="signupbutton">
+                        <input type="submit" class="btn btn-success btn-lg" name="upload" value="Submeter">
+                    </div>
+
+
+                </form>
+
+            </div>
+
+        </div>
+</body>
+
+</html>
