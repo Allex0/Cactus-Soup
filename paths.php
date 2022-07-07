9175ae2d640444b370f208450bfa100895cc9f0b
Alexandru Arseni <59446100+Allex0@users.noreply.github.com>
2022-07-07 14:59:55 +0100

    defined top_seris path

--- paths.php
+++ paths.php
@@ -4,4 +4,5 @@
     define("main", "/cactus-soup/main.php");
     define("logout", "/cactus-soup/logout.php");
     define("top_filmes", "/cactus-soup/top_filmes.php");
+    define("top_seris", "/cactus-soup/top_seris");
 ?>
